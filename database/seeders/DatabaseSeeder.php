9758eb573d590290ea81b2096e78baa2d46cf8a5
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-09-09 04:15:26 +0300

    Initial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Listing;
+use PharIo\Manifest\Email;
+use Illuminate\Database\Seeder;
+use Database\Factories\listingFactory;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+            //\App\Models\User::factory(10)->create();
+            Listing::factory(6)->create();
+
+            // Listing::create(
+            //     [
+            //         'title' => 'Software Engineer',
+            //         'tags'=>'laravel,php',
+            //         'company' => 'modern solutions',
+            //         'location' => 'newyork/usa',
+            //         'email'=>'jack@ex.com',
+            //         'website' => 'http://ex.com',
+            //         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            //         Nemo beatae natus incidunt blanditiis,
+            //         at exercitationem provident aliquid vitae molestiae,
+            //         delectus quia deleniti dolore consectetur.
+            //         Magnam quas perferendis voluptatibus! Dicta, facere!',
+            //     ]
+            // );
+            // Listing::create(
+            //     [
+            //         'title' => 'Security Engineer',
+            //         'tags'=>'Scanning,vulnerability',
+            //         'company' => 'tech art',
+            //         'location' => 'florida/usa',
+            //         'email'=>'mk_a@ex.com',
+            //         'website' => 'http://asx.com',
+            //         'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            //         Nemo beatae natus incidunt blanditiis,
+            //         at exercitationem provident aliquid vitae molestiae,
+            //         delectus quia deleniti dolore consectetur.
+            //         Magnam quas perferendis voluptatibus! Dicta, facere!',
+            //     ]
+            // );
+            
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
---
+++ resources/views/listings.blade.php
@@ -0,0 +1,12 @@
+<a href="/">home</a>   
+
+<h2>
+    {{$listings['title']}}
+</h2>
+
+<p>
+    {{$listings['description']}}
+</p>
+
+
+
