3e1e0eee363104a95ca89166135f71f995e676e4
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-09-10 07:09:10 +0300

    some fornt

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -18,14 +18,14 @@
 <div class="flex">
     <img
         class="hidden w-48 mr-6 md:block"
-        {{-- src="images/acme.png" --}}
+        src="images/logo_transparent.png"
         alt=""
     />
     <div>
         <h3 class="text-2xl">
-            <a href="show.html">Senior Laravel Developer</a>
+            <a href="show.html">{{$l->title}}</a>
         </h3>
-        <div class="text-xl font-bold mb-4">Acme Corp</div>
+        <div class="text-xl font-bold mb-4">{{$l->company}}</div>
         <ul class="flex">
             <li
                 class="flex items-center justify-center bg-black text-white rounded-xl py-1 px-3 mr-2 text-xs"
@@ -49,8 +49,7 @@
             </li>
         </ul>
         <div class="text-lg mt-4">
-            <i class="fa-solid fa-location-dot"></i> Boston,
-            MA
+            <i class="fa-solid fa-location-dot"></i>  {{$l->location}}
         </div>
     </div>
 </div>
