a0a125666ed9f3098214fedbfe8e744d7dd32aec
Ahmed Bdiwy <ahmedbedawi22@gmail.com>
2023-09-14 19:55:50 +0300

    some refactoring

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,12 @@
 use App\Models\Listing;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\listingControler;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
+
+
+Route::group(['prefix'=>'listings'], function(){
+
+    
 // All listings
 Route::get('/',  [listingControler::class,'index']); // routes
 
@@ -29,3 +25,8 @@ Route::post('/listings',[listingControler::class,'store']);
 
 
 Route::get('/listings/{listings}',[listingControler::class,'show']);
+
+
+});
+
+
